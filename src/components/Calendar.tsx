db8565fbf2b4f7dd25be7e5ebd4cf122a5dce2c0
naveen.singh <naveen.singh@itelasoft.com>
2024-08-28 17:45:40 +0530

    minor styles update

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -139,12 +139,12 @@ const CalendarComponent = () => {
               Months <MdArrowDropDown className="ml-1" />
             </button>
             {isDropdownOpen && (
-              <div className="absolute right-0 mt-2 bg-white text-gray-800 rounded-lg shadow-lg z-10">
+              <div className="absolute right-0 mt-2 bg-slate-800 text-gray-200 rounded-lg shadow-lg z-10">
                 {monthsList.map((month, index) => (
                   <button
                     key={month}
                     onClick={() => handleMonthJump(index)}
-                    className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
+                    className="block px-4 py-2 hover:bg-gray-500 w-full text-left"
                   >
                     {month}
                   </button>
